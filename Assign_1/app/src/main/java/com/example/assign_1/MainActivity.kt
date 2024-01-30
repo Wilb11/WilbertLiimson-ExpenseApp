579130cd2b9a4bd9d145a88717eaf40028dbc594
Wilb11 <wilbertnlimson@gmail.com>
2024-01-29 20:33:14 -0500

    updated code by using Toast

--- Assign_1/app/src/main/java/com/example/assign_1/MainActivity.kt
+++ Assign_1/app/src/main/java/com/example/assign_1/MainActivity.kt
@@ -2,20 +2,29 @@ package com.example.assign_1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import android.widget.Button
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var trueButton: Button
+    private lateinit var textViewStatus: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Find the button and text view in the layout
-        val trueButton = findViewById<Button>(R.id.truebutton)
-        val textViewStatus = findViewById<TextView>(R.id.text_view_status)
+        // Initialize the button and text view
+        trueButton = findViewById(R.id.truebutton)
+        textViewStatus = findViewById(R.id.text_view_status)
 
-        // Set up a click listener for the button to change the text view's text
+        // Set up a click listener for the button to show a Toast message
         trueButton.setOnClickListener {
+            Toast.makeText(
+                this,
+                getString(R.string.hello_world),
+                Toast.LENGTH_SHORT
+            ).show()
             textViewStatus.text = getString(R.string.hello_world)
         }
     }
